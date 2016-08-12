1f0cd9453d416d36cf88cc795b85498e7c9d5d75
Vojtěch Kozák <kozak.v@gmail.com>
2016-08-12 10:31:58 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # is-time-to-run
+
+## Motivation
+I run a script which checks availability of websites. This script is run in crone every 5 minutes. If any of the website is unavailable I need to send an e-mail. But I don't want to send this e-mal every 5 minutes but every hour.
+
+This module allows to check if certain period of time has passed and allow your script to run again. It aims at stateless scripts (that don't run in loop as a daemon) and for this purpose it utilises a helper file where it stores the last time your script has run. 
+
+## API
+### isTime(callback)
+This method will return information if your code can be executed again. If it can it will change the helper file to the current time.
+
+## Usage
+
+```javascript
+var IsTimeToRun = require('is-time-to-run');
+
+var timeChecker = new IsTimeToRun({
+    path: 'tmp/',   # default ''
+    period: '30',   # default 60
+    unit: 'minutes' # default 'minutes'
+});
+
+timeChecker.isTime((err, canRun) => {
+    
+});
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+var fs = require("fs");
+var moment = require('moment');
+
+const FILE_NAME = ".isTimeToRun";
+
+function IsTimeToRun(opts) {
+    this.path = opts.path || '';
+    this.period = opts.period || 60;
+    this.unit = opts.unit || 'minutes';
+}
+
+IsTimeToRun.prototype.isTime = function (callback) {
+    let path = this.path;
+
+    readTime(path, (err, data) => {
+        if (err && err.errno === -4058) {
+            return updateTime(path, () => callback(null, true));
+        }
+
+        if (new moment() > new moment(data).add(this.period, this.unit)) {
+            return updateTime(path, () => callback(null, true));
+        }
+
+        callback(null, false);
+    })
+};
+
+IsTimeToRun.prototype.lastRun = readTime;
+
+function updateTime(path, callback) {
+    fs.writeFile(path + FILE_NAME, new moment().format(), (err) => {
+        if (err) throw 'Error IO';
+        callback();
+    });
+}
+
+function readTime(path, callback) {
+    fs.readFile(path + FILE_NAME, (err, data) => {
+        if (err && err.errno !== -4058) throw exception;
+
+        return data ? callback(err, data.toString()) : callback(err, null);
+    });
+}
+
+module.exports = IsTimeToRun;
